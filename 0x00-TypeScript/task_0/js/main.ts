9ccf1ace35d55700fdb5e5a61a2819079c394c84
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2021-06-21 11:36:50 +0100

    Update main.ts

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -4,13 +4,13 @@ interface Student {
     age: number;
     location: string;
 }
-const student1: Student = {
+let student1: Student = {
     firstName: "khaoula",
     lastName: "Derouiche",
     age: 25,
     location: "Hammamet"
 };
-const student2: Student = {
+let student2: Student = {
     firstName: "Oumaima",
     lastName: "Merhbene",
     age: 24,
@@ -18,23 +18,18 @@ const student2: Student = {
 };
 const studentsList: Array <Student> = [student1, student2];
 
-const table = document.createElement("table");
-const thead = document.createElement("thead");
-const tbody = document.createElement("tbody");
+let table = document.createElement("table");
+let thead = document.createElement("thead");
+let tbody = document.createElement("tbody");
 studentsList.forEach((object) => {
-    const row = document.createElement('tr');
-    const row1 = document.createElement('tr');
-    const name = document.createElement('td');
-    const location = document.createElement('td');
+    let row = document.createElement('tr');
+    let name = document.createElement('td');
+    let location = document.createElement('td');
     name.textContent = object.firstName;
     location.textContent = object.location;
     row.appendChild(name);
     row.appendChild(location);
-    row1.appendChild("Name");
-    row1.appendChild("Location");
-    thead.appendChild(row1);
     tbody.appendChild(row);
 });
-table.appendChild(thead);
 table.appendChild(tbody);
-document.body.append(table);
+document.body.appendChild(table);
