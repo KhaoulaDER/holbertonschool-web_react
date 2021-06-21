e3b0d3e6c9e6f7d280cad4289aca02e03fdd7a2d
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2021-06-21 15:53:39 +0100

    Add files via upload

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -18,8 +18,8 @@ const teacher3: Teacher = {
 console.log(teacher3);
 
 interface Directors extends Teacher {
-    numberOfReports : number;
-};
+    numberOfReports: number;
+}
 const director1: Directors = {
     firstName: 'John',
     lastName: 'Doe',
