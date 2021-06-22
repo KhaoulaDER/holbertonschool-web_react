0c33b97e70d361dc8379487439c30a525163fa40
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2021-06-22 15:52:13 +0100

    Add files via upload

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -50,7 +50,7 @@ interface StudentClassInterface {
 interface StudentConstructor {
     new(firstName: string, lastName: string): StudentClassInterface;
 }
-const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface{
+export const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface{
     firstName: string;
     lastName: string;    
     constructor(firstName: string, lastName: string){
@@ -66,6 +66,9 @@ const StudentClass: StudentConstructor = class StudentClass implements StudentCl
     }
 
 }
+const student: StudentClass  new StudentClass("khaled", "Derouiche");
+console.log(student.workOnHomework());
+console.log(student.displayName());
 
 
 
