19307bbe4297d83cac5a27088ede2b86e4a7caa6
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2021-06-23 13:53:58 +0100

    Add files via upload

--- 0x00-TypeScript/task_3/js/main.ts
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -1,5 +1,6 @@
 /// <reference path = "./crud.d.ts" /> ///
-import * as CRUD from "./crud";
+import {RowID, RowElement} from "./interface";
+import * as CRUD from "./crud.js";
 
 
 const row: RowElement = {
