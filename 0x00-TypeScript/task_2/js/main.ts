7c4fec9477a620e6e8e03ab89ad57bcfd389c655
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2021-06-23 10:40:32 +0100

    Add files via upload

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -22,13 +22,13 @@ export class Director implements DirectorInterface {
 }
 export class Teacher implements TeacherInterface {
     workFromHome(): string {
-      return ("Cannot work from home");
+        return ("Cannot work from home");
     }
     getCoffeeBreak(): string {
-      return ("Cannot have a break");
+        return ("Cannot have a break");
     }
     workTeacherTasks(): string {
-      return ("Getting to work");
+        return ("Getting to work");
     }
 }
 export function createEmployee(salary: number | string): Director | Teacher {
@@ -42,7 +42,20 @@ console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
 
+function isDirector(employee: Director | Teacher): employee is Director {
+    return (employee instanceof Director) ? true : false;
+
+}
+
+function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+        return (employee.workDirectorTasks());
+    }
+    else {
+        return (employee.workTeacherTasks());
+    }
+}
 
 
-
-
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
